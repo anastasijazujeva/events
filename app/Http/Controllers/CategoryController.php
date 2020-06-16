7eac0ceb19e1a007c8159defb8d8c600e92a6f08
Anastasija <anastasija130299@gmail.com>
2020-06-16 14:39:05 +0300

    sorted categories in alphabetical order

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::all();
+        $categories = Category::orderBy('category', 'asc')->get();
         return view('categories', ['categories' => $categories]);
     }
 
